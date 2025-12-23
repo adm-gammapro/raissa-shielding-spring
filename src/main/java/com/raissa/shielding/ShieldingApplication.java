31834fd6343eb7df4b60e4ba87a9f4384147c533
Gamma Pro <adm.gammapro@gmail.com>
2025-12-23 11:47:45 -0500

    commit inicial

---
+++ src/main/java/com/raissa/shielding/ShieldingApplication.java
@@ -0,0 +1,13 @@
+package com.raissa.shielding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raissa/shielding/ShieldingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raissa.shielding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
